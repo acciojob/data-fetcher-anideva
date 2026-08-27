1b143d8a7a3ef1ebd76c95d3cc148c78a0599e46
anideva <anidevarchowdhury@gmial.com>
2026-08-27 13:14:55 +0530

    fetch data using API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, {useEffect, useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData]= useState(null)
+
+  useEffect(()=> {
+    const fetchData= async () =>{
+      const responce = await fetch('https://dummyjson.com/products')
+      const result = await responce.json()
+
+      setData(result)
+    }
+    fetchData()
+  }, [])
   return (
-    <div>
+    <div id="main">
         {/* Do not remove the main div */}
+        <pre>{JSON.stringify(data,null,2)}</pre>
     </div>
   )
 }
